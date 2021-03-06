b938957148b9ef5a3678c1bf32b877dfc6423829
Virginia Wu <virginiawu11@gmail.com>
2021-03-06 11:21:48 -0500

    added missing mergeObjects function

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ const filterOutOdds = (...nums) => nums.filter((n) => n % 2 === 0);
 
 const findMin = (...nums) => nums.reduce((ac, nVal) => (ac < nVal ? ac : nVal));
 
+//Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
+const mergeObjects = (obj1, obj2) => ({ ...obj1, ...obj2 });
+
 //Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
 
 const doubleAndReturnArgs = (arr, ...nums) => [ ...arr, ...nums.map((n) => n * 2) ];
